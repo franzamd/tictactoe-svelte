5644af352eb61299a3a6c5f1849cf28790021629
franz medrano <franz.medrano@icloud.com>
2020-08-27 11:11:14 -0400

    first commit

---
+++ src/Space.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let space = "";
+</script>
+
+<style>
+  .space {
+    border: solid 1px gray;
+    height: 125px;
+    width: 125px;
+    flex: 1;
+    font-size: 75px;
+    text-align: center;
+    cursor: pointer;
+  }
+</style>
+
+<div class="space">{space}</div>
